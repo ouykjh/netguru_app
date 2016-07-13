00cdce80379bc97bebf94645d289ad9462c20f59
Kuba Wilczek <jak_wil@wp.pl>
2016-07-13 16:30:19 +0200

    add access restriction to controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+  before_action :authenticate_user!, :unless => :devise_controller?
+
   decent_configuration { strategy DecentExposure::StrongParametersStrategy }
 
   protected
+
+  def authenticate_user!
+    if user_signed_in?
+      super
+    else
+      redirect_to new_user_session_path
+    end
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) do |u|
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root to: 'visitors#index'
+
   resources :students do
     get :subjects
   end
