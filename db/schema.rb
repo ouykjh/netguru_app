d10d4bc17850e5fa6a1636854539d2d775961163
Kuba Wilczek <jak_wil@wp.pl>
2016-07-13 16:28:23 +0200

    add missing attributes in models and fix database schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150719115626) do
+ActiveRecord::Schema.define(version: 20160713141928) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,9 +38,9 @@ ActiveRecord::Schema.define(version: 20150719115626) do
     t.integer  "subject_item_id"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.integer  "student_id"
   end
 
-  add_index "subject_item_notes", ["student_id"], name: "index_subject_item_notes_on_student_id", using: :btree
   add_index "subject_item_notes", ["subject_item_id"], name: "index_subject_item_notes_on_subject_item_id", using: :btree
 
   create_table "subject_items", force: :cascade do |t|
@@ -80,7 +80,6 @@ ActiveRecord::Schema.define(version: 20150719115626) do
 
   add_foreign_key "participations", "students"
   add_foreign_key "participations", "subject_items"
-  add_foreign_key "subject_item_notes", "students"
   add_foreign_key "subject_item_notes", "subject_items"
   add_foreign_key "subject_items", "teachers"
 end
