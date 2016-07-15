4420436ecada58089f5ef5a5c2b1edd9d2ac73d3
Kuba Wilczek <jak_wil@wp.pl>
2016-07-15 10:29:14 +0200

    update schema and student model - add student_id to subject_item_notes (should be added migration but in this case db:schema:load will be not working cause of index on non existing attribute)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160713141928) do
+ActiveRecord::Schema.define(version: 20150719115626) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,12 +34,14 @@ ActiveRecord::Schema.define(version: 20160713141928) do
   end
 
   create_table "subject_item_notes", force: :cascade do |t|
+    t.integer  "student_id"
     t.integer  "value"
     t.integer  "subject_item_id"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
 
+  add_index "subject_item_notes", ["student_id"], name: "index_subject_item_notes_on_student_id", using: :btree
   add_index "subject_item_notes", ["subject_item_id"], name: "index_subject_item_notes_on_subject_item_id", using: :btree
 
   create_table "subject_items", force: :cascade do |t|
@@ -79,6 +81,7 @@ ActiveRecord::Schema.define(version: 20160713141928) do
 
   add_foreign_key "participations", "students"
   add_foreign_key "participations", "subject_items"
+  add_foreign_key "subject_item_notes", "students"
   add_foreign_key "subject_item_notes", "subject_items"
   add_foreign_key "subject_items", "teachers"
 end
